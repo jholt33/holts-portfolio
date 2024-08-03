f37a2827a5353329f0ad31a10245c79fe08bb206
Jeremy Holt <jholt@frontsidecode.com>
2024-08-03 08:42:05 -0700

    update homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,7 +14,7 @@ function Home() {
     >
       <div className="homepg-hdr">
         <div className="container d-flex flex-column text-center">
-          <h1 className="display-1">Welcome to Holt's Code</h1>
+          <h1 className="display-1">Welcome to Holt's Code!</h1>
           <p>
             You have reached the online portfolio of Jeremy Holt - Sr. UI/UX
             Developer<br></br>Scroll down to view some of my past web projects
